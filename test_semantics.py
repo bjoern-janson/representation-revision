cce8bd5ed46e07a7a0faf0496c91e7c783711e8e
bjarki <bjoernjanson.de@gmail.com>
2026-09-03 14:29:50 +0200

    test: correct Boolean semantic signature fixture

--- test_semantics.py
+++ test_semantics.py
@@ -15,18 +15,17 @@ def test_semantic_quotient_distinguishes_syntax_from_meaning():
 
 
 def test_canonical_identity_implies_same_semantics():
-    in0 = make(Op.INPUT, 0)
-    in1 = make(Op.INPUT, 1)
-
-    left = make(Op.EQ, in0, in1)
-    right = make(Op.EQ, in1, in0)
+    left = make(Op.EQ, make(Op.INPUT, 0), make(Op.INPUT, 1))
+    right = make(Op.EQ, make(Op.INPUT, 1), make(Op.INPUT, 0))
 
     assert left.canonical_serialize() == right.canonical_serialize()
     assert semantic_signature(left, d=4) == semantic_signature(right, d=4)
 
 
 def test_semantic_signature_is_exhaustive_and_stable():
-    node = make(Op.XOR, make(Op.EQ, make(Op.INPUT, 0), make(Op.INPUT, 0)), make(Op.INPUT, 1))
+    eq = make(Op.EQ, make(Op.INPUT, 0), make(Op.INPUT, 0))
+    neq = make(Op.NEQ, make(Op.INPUT, 1), make(Op.INPUT, 1))
+    node = make(Op.XOR, eq, neq)
 
     signature_a = semantic_signature(node, d=2)
     signature_b = semantic_signature(node, d=2)
