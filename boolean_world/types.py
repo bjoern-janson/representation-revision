0deb2a842a13f400df93ee67d7506ee8a9d7f096
bjarki <bjoernjanson.de@gmail.com>
2026-09-03 14:28:18 +0200

    fix: align Boolean ontology with constructible AST

--- boolean_world/types.py
+++ boolean_world/types.py
@@ -1,27 +1,20 @@
-# boolean_world/types.py
 from enum import Enum, auto
+
 
 class ValueType(Enum):
     BOOL = auto()
     BIT = auto()
-    INDEX = auto()
-    CONTEXT = auto()
+
 
 class Op(Enum):
-    # Access
     INPUT = auto()
-    CTX = auto()
-    # Boolean operators
     NOT = auto()
     AND = auto()
     OR = auto()
     XOR = auto()
-    # Value relations
     EQ = auto()
     NEQ = auto()
-    # Index relations
-    BEFORE = auto()
-    ADJACENT = auto()
+
 
 class AssayOutcome(Enum):
     REVISE = auto()
